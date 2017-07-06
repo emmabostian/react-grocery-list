d0cf69b7ec1efcd292ca702d3ada7e66343c91c9
Emma Bostian <ebostia@us.ibm.com>
2017-07-06 14:22:30 -0400

    Adding a state switcher example

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ function List(props){
 
 class AddItem extends React.Component {
 	render() {
+		console.log("Add item ", this.props.items);
 		return (
 			<div>
 				<input id="itemInput" type="text"></input>
@@ -42,17 +43,59 @@ class App extends React.Component {
 			empty: false,
 		};
 	}
+
 	render() {
 		return (
 			<div>
 				<List empty={this.state.empty} items={this.state.items}/>
-				<AddItem />
+				<AddItem items={this.state.items}/>
 			</div>
 		);
 	}
 }
 
+class Cards extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			blue: true,
+		}
+	}
+
+	changeState() {
+		this.setState({'blue': !this.state.blue});
+	}
+
+	renderBlue() {
+		return (
+			<div className="card__blue">
+				<h1>Card is blue!</h1>
+				<p>Click to make green</p>
+				<button onClick={this.changeState.bind(this)}>Click Me!</button>
+			</div>
+		);
+	}
+
+	renderGreen() {
+		return (
+			<div className="card__green">
+				<h1>Card is green!</h1>
+				<p>Click to make blue</p>
+				<button onClick={this.changeState.bind(this)}>Click Me!</button>
+			</div>
+		);
+	}
+
+	render() {
+		if(this.state.blue){
+			return this.renderBlue();
+		} else {
+			return this.renderGreen();
+		}
+	}
+}
+
 ReactDOM.render(
-	<App />,
+	<Cards />,
 	document.getElementById('root')
 );
