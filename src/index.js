dff1fe43be5d68279c0f96874c815d0928a94e6a
Emma Bostian <ebostia@us.ibm.com>
2017-07-06 13:01:49 -0400

    Adding grocery list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function ListItem(props){
+	return (
+		<li className="listItem">{props.value}</li>
+	);
+}
+
+class List extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			items: ['Chicken', 'Salsa', "Wine"],
+			empty: false,
+		};
+	}
+
+	renderItem(i) {
+		return (
+			<ListItem
+				value={this.state.items[i]}
+			/>
+		);
+	}
+
+	render() {
+		const empty = this.state.empty;
+		let status;
+		status = empty ? "Add items to get started!" : "Keep adding!";
+
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<ul>
+					{this.renderItem(0)}
+					{this.renderItem(1)}
+					{this.renderItem(2)}
+				</ul>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<List />,
+	document.getElementById('root')
+);
